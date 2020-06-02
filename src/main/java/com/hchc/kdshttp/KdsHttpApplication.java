1e41f32b5bebf12656ac6e7deb3c7d88163323c0
wangrong <1096371950@qq.com>
2020-06-02 15:55:04 +0800

    first commit

---
+++ src/main/java/com/hchc/kdshttp/KdsHttpApplication.java
@@ -0,0 +1,13 @@
+package com.hchc.kdshttp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdsHttpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KdsHttpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hchc/kdshttp/KdsHttpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hchc.kdshttp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KdsHttpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
